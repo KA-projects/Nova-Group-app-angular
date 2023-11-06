a57e7e28d07b6e69b06288687a838632e5d5f429
Edward <122209044+KA-projects@users.noreply.github.com>
2023-11-06 23:13:42 +0600

    added tailwindcss and create architectural methodologies(FSD), start do navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from 'src/layers/pages/home.component';
+import { NavbarComponent } from 'src/layers/entities/ui/navbar/navbar.component';
+import { UserSvgComponent } from 'src/layers/entities/ui/icons/user/userIcon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    //svg icons
+    UserSvgComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
